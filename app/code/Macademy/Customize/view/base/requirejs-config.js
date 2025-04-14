9a25d573147cc2e4e2e9bfb4f19cc2f33b54b7ed
Aayush <aayush.singh@hbwsl.com>
2025-04-14 11:18:21 +0530

    add modal mixin

---
+++ app/code/Macademy/Customize/view/base/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+    config: {
+        mixins: {
+          
+            'Magento_Ui/js/modal/modal': {
+                'Mageplaza_Customize/js/modal-widget-mixin': true
+            },
+           
+        }
+    }
+    
+}    
